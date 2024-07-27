a5c202b83cf8fce7cb8595f406dfc39d56be43e5
DevJaGz <jgomezpersonal7@gmail.com>
2024-07-27 18:29:28 -0500

    refactor: create devtools panel in onInstalled callback

--- background.js
+++ background.js
@@ -32,29 +32,31 @@ chrome.runtime.onInstalled.addListener(() => {
     buttons: [{ title: "Ok" }],
     priority: 0,
   });
-});
 
-chrome.contextMenus.create({
-  id: "openDevTools",
-  title: "Open Store",
-  contexts: ["all"],
-});
-
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === "openDevTools") {
-    chrome.windows.getCurrent({ populate: true }, (currentWindow) => {
-      const width = 800;
-      const height = 600;
-      const left = Math.floor((currentWindow.width - width) / 2);
-      const top = Math.floor((currentWindow.height - height) / 2);
-      chrome.windows.create({
-        url: "panel.html",
-        type: "popup",
-        width,
-        height,
-        left,
-        top,
+  chrome.contextMenus.create({
+    id: "openDevTools",
+    title: "Open Store",
+    contexts: ["all"],
+  });
+  
+  chrome.contextMenus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId === "openDevTools") {
+      chrome.windows.getCurrent({ populate: true }, (currentWindow) => {
+        const width = 800;
+        const height = 600;
+        const left = Math.floor((currentWindow.width - width) / 2);
+        const top = Math.floor((currentWindow.height - height) / 2);
+        chrome.windows.create({
+          url: "panel.html",
+          type: "popup",
+          width,
+          height,
+          left,
+          top,
+        });
       });
-    });
-  }
+    }
+  });
 });
+
+
